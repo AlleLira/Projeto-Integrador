108bb4898d02403e7372d7d377b501e9006aecc1
Alessandra Lira <allelirads@gmail.com>
2024-03-29 11:46:20 -0300

    CSS e JS págians Index, Plano de estudos, Aulas

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+
+window.onload = function() {
+    loadVideo('kx_r8NXD6GA');
+};
+
+function loadVideo(videoId) {
+    document.getElementById('videoPlayer').src = 'https://www.youtube.com/embed/' + videoId;
+};
+
